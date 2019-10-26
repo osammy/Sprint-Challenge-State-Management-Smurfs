cf777c5fd6388aff66c28355c8e7e66b2b6305cd
samuel <osamaimafidon@gmail.com>
2019-10-26 04:48:04 +0100

    finished mvp and one strectch task

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import thunk from "redux-thunk";
+import { combineReducers, createStore, compose, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import * as reducers from './state/reducers';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const allReducers = combineReducers({
+    smurfs: reducers.smurfReducer,
+    formData: reducers.inputFormReducer
+  });
+
+const store = createStore(
+  allReducers,
+    {},
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
---
+++ smurfs/src/state/actionTypes.js
@@ -0,0 +1,5 @@
+export const GET_SMURFS = 'GET_SMURFS';
+export const ON_INPUT_CHANGE = 'ON_INPUT_CHANGE';
+export const ADD_SMURF = 'POST_SMURF';
+export const UPDATE_SMURF = 'UPDATE_SMURF';
+export const DELETE_SMURF = 'DELETE_SMURF';
